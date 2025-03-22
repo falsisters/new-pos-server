230e198a3fa95f63a8a654d1433a73d9f04258c8
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2025-03-22 13:04:11 +0800

    build: add post install script for prisma

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
