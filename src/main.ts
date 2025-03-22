a1523641c4a7f5fc66c985f34fb42cbecbbc0f8b
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2025-03-22 13:13:38 +0800

    build: add post install script for prisma

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { Express } from 'express';
-
-let server: Express;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,15 +10,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
-  await app.init();
-
-  const expressApp = app.getHttpAdapter().getInstance();
-  return expressApp;
+  await app.listen(process.env.PORT ?? 3001);
 }
-
-export const handler = async (req, res) => {
-  if (!server) {
-    server = await bootstrap();
-  }
-  return server(req, res);
-};
+bootstrap();
