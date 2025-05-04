e09306f36507fafed72b7471807d36d683ed1031
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2025-05-04 20:55:56 +0800

    fix: add dependent modules for bills

--- src/bills/bills.module.ts
+++ src/bills/bills.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BillsController } from './bills.controller';
 import { BillsService } from './bills.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { CashierModule } from 'src/cashier/cashier.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaModule, CashierModule, AuthModule],
   controllers: [BillsController],
-  providers: [BillsService]
+  providers: [BillsService, PrismaService, JwtService],
 })
 export class BillsModule {}
