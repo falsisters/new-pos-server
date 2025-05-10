488ea194e0731d92f4d8786315b00033c0b744c0
AJ Aparicio <aparicio.anthonyjohn@gmail.com>
2025-05-10 18:29:43 +0800

    fix: unprotect register

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -25,7 +25,6 @@ export class CustomerController {
     return this.customerService.login(req.user);
   }
 
-  @UseGuards(JwtCustomerAuthGuard)
   @Post('register')
   async register(@Body() registerCustomerDto: RegisterCustomerDto) {
     return this.customerService.register(registerCustomerDto);
